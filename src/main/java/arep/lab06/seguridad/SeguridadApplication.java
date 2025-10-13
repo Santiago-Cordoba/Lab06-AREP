a638f87f20bcdd379c26dc6a5dae63c744be6eb6
Santiago-Cordoba <santiagocordobaduenas@gmail.com>
2025-10-13 12:59:45 -0500

    back inicio

---
+++ src/main/java/arep/lab06/seguridad/SeguridadApplication.java
@@ -0,0 +1,13 @@
+package arep.lab06.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadApplication.class, args);
+	}
+
+}
